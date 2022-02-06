6d3258428c3773a8aa8c0a946cb58d9f8655691a
ekypow <lucas.schrever@gmail.com>
2022-02-06 20:37:58 +0100

    4 quiz terminés

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import App from './App';
 import Home from './pages/Home';
 import QuizGeo from './pages/QuizGeo'
 import QuizSport from './pages/QuizSport'
+import QuizRap from './pages/QuizRap'
+import QuizScience from './pages/QuizScience'
 import NoPage from './pages/NoPage'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -13,8 +15,10 @@ export default function Routing() {
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
-          <Route path="/quiz-pays-et-capitales" element={<QuizGeo />} />
-          <Route path="/quiz-surnom-de-joueurs" element={<QuizSport />} />
+          <Route path="/pays-et-capitales" element={<QuizGeo />} />
+          <Route path="/surnom-de-joueurs" element={<QuizSport />} />
+          <Route path="/rappeurs" element={<QuizRap />} />
+          <Route path="/symboles-chimiques" element={<QuizScience />} />
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
