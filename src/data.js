44206da5d0dab9e7a932f4c3570e281a3c86fb44
ekypow <lucas.schrever@gmail.com>
2022-02-06 16:30:08 +0100

    Quiz on

--- src/data.js
+++ src/data.js
@@ -1,60 +1,60 @@
 export const data = [
     {
-        id: '1',
+        id: '0',
         question: 'Quelle est la capitale du Pérou ?',
         answer: 'Lima',
         propositions: ['Rome', 'Lima', 'Quito']
     },
     {
-        id: '2',
+        id: '1',
         question: "Quelle est la capitale de l'Égypte ?",
         answer: 'Le Caire',
         propositions: ['Suva', 'Tallin', 'Le Caire']
     },
     {
-        id: '3',
+        id: '2',
         question: 'Quelle est la capitale du Djibouti ?',
         answer: 'Djibouti',
         propositions: ['Djibouti', 'Roseau', 'Asmara']
     },
     {
-        id: '4',
+        id: '3',
         question: 'Quelle est la capitale de la Hongrie ?',
         answer: 'Budapest',
         propositions: ['Budapest', 'Jakarta', 'Bagdad']
     },
     {
-        id: '5',
+        id: '4',
         question: 'Quelle est la capitale du Kenya ?',
         answer: 'Nairobi',
         propositions: ['Téhéran', 'Tokyo', 'Nairobi']
     },
     {
-        id: '6',
+        id: '5',
         question: 'Quelle pays a pour capitale Vilnius ?',
         answer: 'Lituanie',
         propositions: ['Lituanie', 'Libéria', 'Lettonie']
     },
     {
-        id: '7',
+        id: '6',
         question: 'Quelle pays a pour capitale Riga ?',
         answer: 'Lettonie',
         propositions: ['Lesotho', 'Libye', 'Lettonie']
     },
     {
-        id: '8',
+        id: '7',
         question: 'Quelle pays a pour capitale Kuala Lumpur ?',
         answer: 'Malaisie',
         propositions: ['Macédoine', 'Malaisie', 'Madagascar']
     },
     {
-        id: '9',
+        id: '8',
         question: 'Quelle pays a pour capitale Asunción ?',
         answer: 'Paraguay',
         propositions: ['Paraguay', 'Pérou', 'Philippines']
     },
     {
-        id: '10',
+        id: '9',
         question: 'Quelle pays a pour capitale Stockholm ?',
         answer: 'Suède',
         propositions: ['Suisse', 'Suède', 'Suriname']
