b5a82a8a8517c72398e5ff9a143875f50fb2e36f
ekypow <lucas.schrever@gmail.com>
2022-02-06 17:04:18 +0100

    2d data file

---
+++ src/json/data-sport.js
@@ -0,0 +1,62 @@
+export const dataSport = [
+    {
+        id: '0',
+        question: 'Quel sportif était surnommé Boum Boum ?',
+        answer: 'Boris Becker',
+        propositions: ['Michael Jordan', 'Boris Becker', 'Jessie Owens', 'Mohammed Ali']
+    },
+    {
+        id: '1',
+        question: "Quel sportif était surnommé le Kid de Las Vegas ?",
+        answer: 'André Agassi',
+        propositions: ['André Agassi', 'Pete Sampras', 'Shaquille O’Neal', 'Michael Phelps']
+    },
+    {
+        id: '2',
+        question: 'Quel footballeur était surnommé le King ?',
+        answer: 'Éric Cantona',
+        propositions: ['Zinedine Zidane', 'Wayne Rooney', 'David Beckham', 'Éric Cantona']
+    },
+    {
+        id: '3',
+        question: 'Quel sportif était surnommé l’Intello ?',
+        answer: 'Laurent Fignon',
+        propositions: ['Alain Prost', 'Laurent Blanc', 'Bernard Laporte', 'Laurent Fignon']
+    },
+    {
+        id: '4',
+        question: 'Quel sportif était surnommé le Vengeur masqué ?',
+        answer: 'Henri Leconte',
+        propositions: ['Henri Leconte', 'John McEnroe', 'Mark Spitz', 'Joe DiMaggio']
+    },
+    {
+        id: '5',
+        question: 'Quel sportif était surnommé « Le Méchant » ?',
+        answer: 'Ilie Nastase',
+        propositions: ['Mike Tyson', 'Diego Maradona', 'Ilie Nastase', 'Romario']
+    },
+    {
+        id: '6',
+        question: 'Quel sportif était surnommé le Pistolet ?',
+        answer: 'Pete Sampras',
+        propositions: ['Pete Sampras', 'Ivan Lendl', 'Jimmy Connors', 'Bjon Borg']
+    },
+    {
+        id: '7',
+        question: 'Quel sportif était surnommé Guga ?',
+        answer: 'Gustavo Kuerten',
+        propositions: ['David Ginola', 'Gustavo Kuerten', 'Gabriel Batistuta', 'Gerard Piqué']
+    },
+    {
+        id: '8',
+        question: 'Quel sportif était surnommé The Juice ?',
+        answer: 'O.J. Simpson',
+        propositions: ['O.J. Simpson', 'Mohammed Ali', 'Michael Schumacher', 'Maradona']
+    },
+    {
+        id: '9',
+        question: 'Quel footballeur était surnommé The Rock ?',
+        answer: 'Marcel Dessailly',
+        propositions: ['Raul', 'Éric Cantona', 'Abedi Pelé', 'Marcel Dessailly']
+    },
+]
